a8296f5d0967e6c0a41189f96fc3f0afe96b3edd
Vladyslav6 <vlad51284@gmail.com>
2024-01-06 23:34:57 +0200

    pushOne

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+
+
+
 export const App = () => {
   return (
     <div
       style={{
+        
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        // backgroundColor: 'green',
+     
       }}
     >
-      React homework template
+      React homework templatedd
     </div>
   );
 };
---
+++ src/components/Profil.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+
+
+export const divin = styled.div`
+background-color: #518080;
+` 
