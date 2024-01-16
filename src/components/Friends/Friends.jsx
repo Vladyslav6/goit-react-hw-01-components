7c4fae698a24ba372870bde28e4a6e3d82f949be
Vladyslav6 <vlad51284@gmail.com>
2024-01-16 22:19:10 +0200

    clear

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -26,33 +26,3 @@ FriendList.propTypes = {
 };
 
 export default FriendList;
-
-// FriendList.propTypes ={
-//   friends: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//     }
-//   )
-// }
-
-// const FriendList = ({ friendsLi }) => {
-//   const FriendMap = friendsLi.map(item => (
-//     <li className={Styles.item} key={item.id}>
-//       <span className={Styles.status}>
-//         {item.isOnline === true ? (
-//           <div className={Styles.coloOnline}></div>
-//         ) : (
-//           <div className={Styles.coloOffline}></div>
-//         )}
-//       </span>
-//       <img
-//         className={Styles.avatar}
-//         src={item.avatar}
-//         alt="User avatar"
-//         width="48"
-//       />
-//       <p className={Styles.name}>{item.name}</p>
-//     </li>
-//   ));
-//   return <ul className={Styles.friendList}>{FriendMap}</ul>;
-// };
--- src/components/Friends/friedsListItem.jsx
+++ src/components/Friends/friedsListItem.jsx
@@ -29,20 +29,3 @@ FriendListItem.propTypes = {
   name: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
 };
-
-// <li className={Styles.item} key={item.id}>
-//   <span className={Styles.status}>
-//     {item.isOnline === true ? (
-//       <div className={Styles.coloOnline}></div>
-//     ) : (
-//       <div className={Styles.coloOffline}></div>
-//     )}
-//   </span>
-//   <img
-//     className={Styles.avatar}
-//     src={item.avatar}
-//     alt="User avatar"
-//     width="48"
-//   />
-//   <p className={Styles.name}>{item.name}</p>
-// </li>;
